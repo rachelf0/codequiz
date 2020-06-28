1841d80502f8834410ad60ddb6667ed3b5202d04
Rachel Fritz <rachelfritz0@gmail.com>
2020-06-28 11:22:00 -0500

    restarted approach and tried a new method for the js file

--- script.js
+++ script.js
@@ -1,75 +1,60 @@
-var displayTimeElem = document.getElementById("displayTime")
-var displayTitleElem = document.getElementById("questionTitle")
-var btn0Elem = document.getElementById("btn-0")
-var btn1Elem = document.getElementById("btn-1")
-var btn2Elem = document.getElementById("btn-2")
-var btn3Elem = document.getElementById("btn-3")
+document.addEventListener('DOMContentLoaded', () => {
+  const timeLeftDisplay = document.querySelector('#time-left')
+  const startButton = document.getElementById('start-btn')
+  const nextButton = document.getElementById('next-btn')
+  const questionContainerElement = document.getElementById('question-container')
+  const questionElement = document.getElementById('question')
+  const answerButtonsElement = document.getElementById('#start-btn')
+  let myVar;
+  let timeLeft = 60
+  let shuffledQuestions, currentQuestionIndex
+  let gameScore = 0;
+  
+  startButton.addEventListener('click', function () {
+    startGame();
+  })
 
-//use interval timer for the time remaining
-let timeRemaining = 60;
-var currentQuestion = 0;
-let gameScore = 0;
-let shuffledQuestions, currentQuestionIndex
+  nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+  })
 
-//Stopwatch
-var myTimer;
-function clock() {
-    myTimer = setInterval(myClock, 1000);
-    function myClock() {
-        document.getElementById("timer").innerHTML = c--;
-        if (c == 0) {
-        clearInterval(myTimer);
-       }
+  function updatedCountdown() {
+    myVar = setInterval(changeClock, 1000)
+    function changeClock () {
+      if (timeLeft <= -1) {
+        timeLeft = 5;
+        return clearInterval(myVar)
+      }
+      console.log(timeLeft);
+      timeLeftDisplay.innerHTML = timeLeft
+      timeLeft -=1
     }
-}
+  }
+})
 
 var questions = [
-    {
-        title:"What is the last letter of the alphabet?",
-        choices:["a","k","z","i"],
-        correct:"z",
-    },
-    {
-        title:"What is the first letter of the alphabet?",
-        choices:["r","k","a","i"],
-        correct:"a",
-    },
-    {
-        title:"What color is the sky?",
-        choices:["red","green","purple","blue"],
-        correct:"blue",
-    },
-    {
-        title:"What is 2+2?",
-        choices:["5","4","1","2"],
-        correct:"4",
-    },
+  {
+    title: "What is the last letter of the alphabet?",
+    choices: ["a", "k", "z", "i"],
+    correct: "z",
+  },
+  {
+    title: "What is the first letter of the alphabet?",
+    choices: ["r", "k", "a", "i"],
+    correct: "a",
+  },
+  {
+    title: "What color is the sky?",
+    choices: ["red", "green", "purple", "blue"],
+    correct: "blue",
+  },
+  {
+    title: "What is 2+2?",
+    choices: ["5", "4", "1", "2"],
+    correct: "4",
+  },
 ]
-currentQuestion = currentQuestion + 2
-
-//if you ever see object object know you have to access a property (with a ".")
-displayTitleElem.textContent = questions[currentQuestion].title
-
-btn0Elem.textContent = questions[currentQuestion].choices
-
-
-//do the same thing with the buttons as above and you won't need any values in the HTML
-
-// goes in the on click for the start quiz
-displayTimeElem.textContent = timeRemaining
-
-
-//Stopwatch
-var myTimer;
-function clock() {
-    myTimer = setInterval(myClock, 1000);
-    function myClock() {
-        document.getElementById("timer").innerHTML = c--;
-        if (c == 0) {
-        clearInterval(myTimer);
-       }
-    }
-}
 
 
 
